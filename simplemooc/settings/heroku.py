2e96b9d0c65709e74e5b81c1764039bba870b31c
PedrinaBrasil <pedrinabrasil@gmail.com>
2022-03-25 11:13:12 -0300

    configuracao heroku

---
+++ simplemooc/settings/heroku.py
@@ -0,0 +1,15 @@
+import os
+import environ
+from simplemooc.settings.base import *
+
+env = environ.Env()
+
+DEBUG = env.bool("DEBUG", False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "default": env.db(),
+}
